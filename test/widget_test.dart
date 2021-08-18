40ce620d9c42c608eaf2d864a81a1ad309bf2084
Damianox <damianox3@gmail.com>
2021-08-18 22:27:48 +0200

    Testing DI and API functions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,45 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:weather_app/main.dart';
+import 'package:weather_app/api/clients/weather_client.dart';
+import 'package:weather_app/api/di/di.dart' as di;
+import 'package:weather_app/api/models/consolidated_weather.dart';
+import 'package:weather_app/api/models/location_model.dart';
+import 'package:get_it/get_it.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  group("WeatherClient test", () {
+    test("Function di.init() completes without exception", () {
+      expect(di.init(), isA<Future<GetIt>>());
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    var weatherClient;
+    test('Returns WeatherClient', () async {
+      weatherClient = di.getIt<WeatherClient>();
+      expect(weatherClient, isA<WeatherClient>());
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    test('Get location returns LocationModel', () async {
+      var rsp = await weatherClient.getLocation(523920);
+      expect(rsp, isA<LocationModel>());
+    });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    var rspLocationModel;
+    test('Returned LocationModel for woeid 523920 contains title Warsaw', () async {
+      rspLocationModel = await weatherClient.getLocation(523920);
+      expect(rspLocationModel.title, "Warsaw");
+    });
+
+    test('Returned LocationModel contains ConsolidatedWeather is list with length == 6 ', () {
+      var x = rspLocationModel as LocationModel;
+      expect(x.consolidatedWeather, isA<List<ConsolidatedWeather>>());
+      expect(x.consolidatedWeather.length, 6);
+    });
+
+    test('Get location returns Exception if calling not existing woeid',
+        () async {
+      expect(weatherClient.getLocation(0), throwsException);
+    });
   });
 }
